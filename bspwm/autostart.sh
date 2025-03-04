3dac98b20760dcdbab5cf0e38cc7075422d29f2c
unix-banker <mikey@debian-home.home>
2025-03-04 21:09:44 +0000

    removed redundant file

--- bspwm/autostart.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-
-killall picom
-picom --config /home/mikey/.config/picom/picom.conf
-
-feh --bg-fill /home/mikey/walla/digital/a_car_driving_on_a_road_at_night.png /home/mikey/walla/digital/a_car_in_a_field_with_trees_and_a_fire.png
