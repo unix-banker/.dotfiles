79d2e0c73cd28d364aa562ec134449c35a9c299f
unix-banker <mikey@debian-home.home>
2025-03-04 21:06:54 +0000

    Added dotfiles

---
+++ bspwm/autostart.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+killall picom
+picom --config /home/mikey/.config/picom/picom.conf
+
+feh --bg-fill /home/mikey/walla/digital/a_car_driving_on_a_road_at_night.png /home/mikey/walla/digital/a_car_in_a_field_with_trees_and_a_fire.png
