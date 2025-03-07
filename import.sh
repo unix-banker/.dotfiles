cfbec3ba51f95959c247cca086335c669cb07e21
unix-banker <189669124+unix-banker@users.noreply.github.com>
2025-03-07 16:44:48 +0000

    fixed directories

--- import.sh
+++ import.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp -r /home/mikey/.config/bspwm ./bspwm
-cp -r /home/mikey/.config/alacritty ./alacritty
-cp -r /home/mikey/.config/picom ./picom
-cp -r /home/mikey/.config/polybar ./polybar
-cp -r /home/mikey/.config/ranger ./ranger
-cp -r /home/mikey/.config/rofi ./rofi
+cp -r /home/mikey/.config/alacritty ./
+cp -r /home/mikey/.config/bspwm ./
+cp -r /home/mikey/.config/picom ./
+cp -r /home/mikey/.config/polybar ./
+cp -r /home/mikey/.config/rofi ./
+cp -r /home/mikey/.config/sxhkd ./
