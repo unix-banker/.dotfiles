2b24702d12eec8657b31b91808485d9b18bc65b7
unix-banker <189669124+unix-banker@users.noreply.github.com>
2025-03-07 16:39:09 +0000

    updated dotfiles

---
+++ import.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp -r /home/mikey/.config/bspwm ./bspwm
+cp -r /home/mikey/.config/alacritty ./alacritty
+cp -r /home/mikey/.config/picom ./picom
+cp -r /home/mikey/.config/polybar ./polybar
+cp -r /home/mikey/.config/ranger ./ranger
+cp -r /home/mikey/.config/rofi ./rofi
